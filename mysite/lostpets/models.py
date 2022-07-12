6304c104b5aeab592219e2658de5efb8b2b5e2e5
mattcole19 <ccmatt19@gmail.com>
2022-07-12 12:20:24 -0400

    Created models and added user profile to admin page

--- mysite/lostpets/models.py
+++ mysite/lostpets/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+
+
+# Extends User model to hold address, phone number, etc
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+
+class Pet(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    gender = models.CharField(max_length=20)
+
+
+class Post(models.Model):
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10)
+    description = models.CharField(max_length=200)
+    area_last_seen = models.CharField(max_length=200)
+
+# Something not working here, will leave it commented out for now
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+#
+#
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
