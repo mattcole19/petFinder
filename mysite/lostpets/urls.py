0e52e205944281d06b818430b92f2a02be31528e
mattcole19 <ccmatt19@gmail.com>
2022-07-12 15:26:36 -0400

    Rough views working to show all psots and ability to click into them

--- mysite/lostpets/urls.py
+++ mysite/lostpets/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('home/', views.home, name="home"),
+    path('<int:post_id>/', views.detail, name="post_detail"),
 ]
--- mysite/lostpets/views.py
+++ mysite/lostpets/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import Post
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, welcome to the LostPets app!")
+    return render(request, 'lostpets/index.html')
+
+
+# Shows 5 most recent posts
+def home(request):
+    latest_posts = Post.objects.all()
+    context = {"posts": latest_posts}
+    return render(request, 'lostpets/home.html', context)
+
+
+# Shows full information for that post
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'lostpets/post.html', {'post': post})
